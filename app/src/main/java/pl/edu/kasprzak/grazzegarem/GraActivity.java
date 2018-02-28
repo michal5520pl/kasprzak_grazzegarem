6ff5491380516f38d523888cdf523e97c086c301
Piotr Marcinkiewicz <p.marcinkiew@samsung.com>
2018-02-28 21:56:35 +0100

    Life cycle logging, fixes
    1. Android log added for life cycle events.
    2. Handler execution in background fixed.

--- app/src/main/java/pl/edu/kasprzak/grazzegarem/GraActivity.java
+++ app/src/main/java/pl/edu/kasprzak/grazzegarem/GraActivity.java
@@ -3,54 +3,77 @@ package pl.edu.kasprzak.grazzegarem;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class GraActivity extends AppCompatActivity {
 
+    static private boolean runningClock = false;
+    static private int counter = 100;
+    private Runnable worker;
+    private Button action;
+    private TextView clock;
+    private Handler handler;
 
-    class ClockWorker implements Runnable {
-        @Override
-        public void run() {
-            clock.setText("" + counter);
-            --counter;
-            if (runningClock) {
-                handler.postDelayed(worker, 1);
-            }
-        }
-    }
-
-    class ButtonClick implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            if (!runningClock) {
-                counter = 100;
-                runningClock = true;
-                handler.postDelayed(worker, 1);
-            } else {
-                runningClock = false;
-            }
-        }
-    }
-    boolean runningClock = false;
-    int counter;
-
-    ClockWorker worker;
-    Button action;
-    ButtonClick click;
-    TextView clock;
-    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gra);
         action = (Button)findViewById(R.id.action);
         clock = (TextView)findViewById(R.id.clock);
-        click = new ButtonClick();
-        worker = new ClockWorker();
+        worker = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("LICZNIK", "DZIAŁAM " + counter);
+                clock.setText("" + counter);
+                --counter;
+                if (runningClock) {
+                    handler.postDelayed(worker, 1);
+                }
+            }
+        };
         handler = new Handler();
-        counter = 100;
-        action.setOnClickListener(click);
+        action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!runningClock) {
+                    counter = 100;
+                    runningClock = true;
+                    handler.postDelayed(worker, 1);
+                } else {
+                    runningClock = false;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(worker);
+        Log.d("CYKL_ZYCIA", "ONPAUSE");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (runningClock) {
+            handler.postDelayed(worker, 1);
+        }
+        Log.d("CYKL_ZYCIA", "ONRESUME");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("CYKL_ZYCIA", "ONSTART");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("CYKL_ZYCIA", "ONSTOP");
     }
 }
